2a67bf2433176cc782fb7fe27b93b3074fd004f6
Murat <120252342+Onexite@users.noreply.github.com>
2024-02-13 22:36:46 +0300

    Add files via upload

---
+++ cred.py
@@ -0,0 +1,49 @@
+# --------------------------------
+# |                              |
+# |      copyright onexite       |
+# |     Developed by onexite     |
+# |      Discord : onexite       |
+# |                              |
+# --------------------------------
+
+admin_chat_id = "CHANGE_ME"
+token = "CHANGE_ME"
+
+mysqlHost = "localhost"
+mysqlUsName = "root"
+mysqlPass = ""
+mysqlDBName = "authorizedtoken"
+
+mysqlHost2 = "localhost:3306"
+
+serverip = "192.168.1.20"
+
+
+def get_mysqli_host():
+    return mysqlHost
+def get_mysqli_name():
+    return mysqlUsName
+def get_mysqli_pass():
+    return mysqlPass
+def get_mysqli_dbname():
+    return mysqlDBName
+
+def get_admin_chat_id():
+    return admin_chat_id
+
+def get_mysqli_host2():
+    return mysqlHost2
+
+def get_token():
+    return token
+
+def get_serverip():
+    return serverip
+
+# --------------------------------
+# |                              |
+# |      copyright onexite       |
+# |     Developed by onexite     |
+# |      Discord : onexite       |
+# |                              |
+# --------------------------------
